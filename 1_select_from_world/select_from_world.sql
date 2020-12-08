23c4620963667ef50c1c74b0b66bc6ba15b32d03
David Bassey <davidosky1@gmail.com>
2020-12-08 11:52:07 +0100

    adds all my answers to project SQL ZOO as .sql files and descriptive readme file

---
+++ 1_select_from_world/select_from_world.sql
@@ -0,0 +1,53 @@
+-- 1
+SELECT name, continent, population FROM world
+
+-- 2
+SELECT name FROM world
+WHERE population >= 200000000
+
+-- 3
+SELECT name, gdp / population FROM world 
+WHERE population >= 200000000;
+
+-- 4
+SELECT name, population / 1000000 FROM world
+WHERE continent = 'South America';
+
+-- 5
+SELECT name, population FROM world 
+WHERE name = 'France' OR name = 'Germany' OR name = 'Italy';
+
+-- 6
+SELECT name FROM world 
+WHERE name LIKE '%United%';
+
+-- 7
+SELECT name, population, area FROM world
+WHERE area >= 3000000 OR population >= 250000000;
+
+-- 8
+SELECT name, population, area FROM world
+WHERE area >= 3000000 XOR population >= 250000000;
+
+-- 9
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) 
+FROM world WHERE continent = 'South America';
+
+-- 10
+SELECT name, ROUND(gdp/population, -3) FROM world
+WHERE gdp >= 1000000000000;
+
+-- 11
+SELECT name, capital
+  FROM world
+WHERE LENGTH(name) = LENGTH(capital);
+
+--  12
+SELECT name, capital 
+from world
+where LEFT(name, 1) = LEFT(capital, 1) and name <> capital;
+
+-- 13 All the vowels
+SELECT name
+  FROM world
+where name LIKE '%a%' and name like '%e%' and name like '%i%' and name like '%o%' and name like '%u%' and name not like '% %'
